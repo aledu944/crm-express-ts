48f365c2d103cf9e1e3b762f1830dc4bf6d5e182
Ultron <duartemattias3@gmail.com>
2023-09-19 15:41:10 -0300

    client service base

---
+++ src/services/client.service.ts
@@ -0,0 +1,13 @@
+const findAll = () => {};
+const findOne = () => {};
+const create = () => {};
+const remove = () => {};
+const update = () => {};
+
+export default {
+    findAll,
+    findOne,
+    create,
+    remove,
+    update,   
+}
